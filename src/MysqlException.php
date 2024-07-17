3a82a8f4610d04bac0fd386af4a617c5b6491e10
Marco <mascarnatto@dpsonline.com.ar>
2024-07-17 11:29:59 -0300

    Evito warnings.

--- src/MysqlException.php
+++ src/MysqlException.php
@@ -5,10 +5,14 @@ namespace Dps;
 use Exception;
 
 class MysqlException extends Exception { 
+    var $MysqlError;
+    var $MysqlNro;
 
     public function __construct($texto, $nro = 0, Exception $previous = null) {
+        parent::__construct($texto, $nro);
         $this->MysqlError = $texto;
         $this->MysqlNro   = $nro;
         error_log("MysqlException: $nro - $texto"); 
+        
     }
 }
